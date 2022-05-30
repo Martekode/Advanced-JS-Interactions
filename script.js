83be51e9aeb18901d0c6b35114ae6398eef0fa05
Martekode <BrianDesmedt9000@hotmail.com>
2022-05-30 12:28:45 +0200

    added discription tot images, removing-problems

--- script.js
+++ script.js
@@ -23,20 +23,28 @@ for (let i = 0; i<imgContainer.length;i++){
     image.classList.add('picture');
     image.addEventListener('click',function(){
         console.log(`jajaze ${i}`);
-        /*const p = document.createElement('p');
+        //imgContainer[i].innerHTML = `<p>this is picture nr:${i} and they are friends.</p>`;
+        const p = document.createElement('p');
         p.style.zIndex = "101";
-        p.innerText = `this is picture nr:${i} and they are friends.`;*/
+        p.innerText = `this is picture nr:${i} and they are friends.`;
+        p.style.color = "black";
+        p.style.position = "absolute";
+        //const paragrave = imgContainer[i].querySelector('p');
+        //paragrave.style.zIndex = "101";
+        //paragrave.style.color = "red";
         this.style.position = "absolute";
         this.style.transform = "scale(1.5)";
         this.style.zIndex = "100";
         //console.log(image[1])
-        //image.appendChild(p);
+        imgContainer[i].appendChild(p);
         this.addEventListener('mouseout',function(){
             this.style.position = "relative";
             this.style.transform = "scale(1)";
             this.style.zIndex = "unset";
-            //console.log(this.p)
-            //this.removeChild(p);
+            if(imgContainer[i].querySelector('p')){
+                imgContainer[i].removeChild(p);
+            }
+            
         })
     })
     imgContainer[i].appendChild(image);
